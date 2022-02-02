3d2cf162513245990220158b33b250d322b2b485
lzarandon <lautaro.zarandon@coderio.co>
2022-02-02 20:17:11 -0300

    updated api timezone

--- src/components/Timezones/timezones.js
+++ src/components/Timezones/timezones.js
@@ -1,105 +1,71 @@
 const app = require("express");
 const handleError = require("../../utils/handleError");
 const {
-  getTimezones,
-  getTimezone,
-  getTimezonesMongo,
-  insertTimezone,
-  deleteTimezone,
+    getTimezones,
+    getTimezone,
+    getTimezonesMongo,
+    insertTimezone,
+    deleteTimezone,
 } = require("./services");
 const router = app.Router({ mergeParams: true });
 
 router
+    .get("/", async (req, res) => {
+        const timezones = await getTimezones();
+        if (!timezones) {
+            return res.status(404).json({
+                success: false,
+                message: "Error get timezones",
+            });
+        }
 
-  .get("/", async (req, res) => {
-    const timezones = await getTimezones();
-    if (!timezones) {
-      return res.status(404).json({
-        success: false,
-        message: "Error get timezones",
-      });
-    }
-
-    const { data } = timezones;
-    return res.status(200).json({
-      success: true,
-      message: "Success get timezones",
-      body: {
-        timezones: data,
-      },
-    });
-  })
-
-  .get(
-    "/saved",
-    handleError(async (req, res) => {
-      const { page, limit } = req.query;
-      const timezones = await getTimezonesMongo(page, limit);
-      console.log(timezones);
-      return res.status(200).json({
-        body: { timezones },
-      });
-    }),
-  )
-
-  .get("/:continent/:country", async (req, res) => {
-    const { continent, country } = req.params;
-    const findTimezone = `${continent}/${country}`;
-
-    const timezones = await getTimezone(findTimezone);
-    if (!timezones) return res.status(404).json({});
-
-    const { data } = timezones;
-    return res.status(200).json({
-      body: {
-        timezone: data,
-      },
-    });
-  })
-
-  .put(
-    "/:continent/:country",
-    handleError(async (req, res) => {
-      const { continent, country } = req.params;
-      const findTimezone = `${continent}/${country}`;
-      const timezone = await getTimezone(findTimezone);
-      if (timezone) {
-        const newTimezone = await insertTimezone(timezone);
+        const { data } = timezones;
         return res.status(200).json({
-          success: true,
-          message: "Congrulations timezone updated",
-          body: newTimezone,
+            success: true,
+            message: "Success get timezones",
+            body: {
+                timezones: data,
+            },
         });
-      }
-    }),
-  )
+    })
 
-  .put(
-    "/:continent/:country/:city",
-    handleError(async (req, res) => {
-      const { continent, country, city } = req.params;
-      const findTimezone = `${continent}/${country}/${city}`;
-      const timezone = await getTimezone(findTimezone);
-      if (timezone) {
-        const newTimezone = await insertTimezone(timezone);
-        return res.status(200).json({
-          body: newTimezone,
-        });
-      }
-    }),
-  )
+    .get(
+        "/favorites",
+        handleError(async (req, res) => {
+            const { page, limit } = req.query;
+            const timezones = await getTimezonesMongo(page, limit);
+            return res.status(200).json(timezones);
+        }),
+    )
 
-  .delete(
-    "/:id",
-    handleError(async (req, res) => {
-      const { id } = req.params;
-      const deleted = await deleteTimezone(id);
-      return res.status(200).json({
-        success: true,
-        message: "Congrulations timezone deleted",
-        body: deleted,
-      });
-    }),
-  );
+    .post(
+        "/:continent/:country/:city",
+        handleError(async (req, res) => {
+            const { continent, country, city } = req.params;
+            const findTimezone = `${continent}/${country}/${city}`;
+            const timezone = await getTimezone(findTimezone);
+            if (timezone) {
+                const newTimezone = await insertTimezone(timezone);
+                return res.status(200).json({
+                    body: newTimezone,
+                    success: true,
+                    message: "Congrulations timezone updated",
+                });
+            }
+        }),
+    )
+
+    .delete(
+        "/:id",
+        handleError(async (req, res) => {
+            const { id } = req.params;
+            const deleted = await deleteTimezone(id);
+            return res.status(200).json({
+                success: true,
+                message: "Congrulations timezone deleted",
+                body: deleted,
+            });
+        }),
+    );
 
 module.exports = router;
--- src/db/mongoConnect.js
+++ src/db/mongoConnect.js
@@ -1,47 +1,38 @@
 const mongoose = require("mongoose");
 
-const {
-  MONGO_USER,
-  MONGO_PASS,
-  MONGO_CLUSTER,
-  MONGO_DB_NAME,
-  NODE_ENV,
-} = process.env;
+const { MONGO_USER, MONGO_PASS, MONGO_CLUSTER, MONGO_DB_NAME, NODE_ENV } =
+    process.env;
 mongoose.Promise = global.Promise;
 mongoose.set("debug", NODE_ENV === "development");
 mongoose.set("useFindAndModify", false);
 
 const options = {
-  autoIndex: false,
-  poolSize: 5,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  user: MONGO_USER,
-  pass: MONGO_PASS,
-  dbName: MONGO_DB_NAME,
+    autoIndex: false,
+    poolSize: 5,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: MONGO_USER,
+    pass: MONGO_PASS,
+    dbName: MONGO_DB_NAME,
 };
 
 let isConected = false;
 
 const connectDB = async () => {
-  try {
-    const URI = `mongodb${
-      NODE_ENV !== "development" ? "+srv" : ""
-    }://${MONGO_CLUSTER}.mongodb.net`;
-    const dbs = await mongoose.connect(URI, options);
-    if (dbs.connections[0].readyState === 1) {
-      console.log("🌱 Connected to MongoDB");
-      isConected = dbs.connections[0].readyState;
+    try {
+        // Connect to the MongoDB cluster
+        mongoose.connect(MONGO_CLUSTER, options, () =>
+            console.log("Mongoose is connected"),
+        );
+    } catch (e) {
+        console.log("could not connect");
     }
-  } catch (err) {
-    throw ("🔥 Error on mongodb conection ", err);
-  }
 };
 
 module.exports.checkConection = async () => {
-  if (isConected === false) {
-    const conection = await connectDB();
-    return conection;
-  }
-  return isConected;
+    if (isConected === false) {
+        const conection = await connectDB();
+        return conection;
+    }
+    return isConected;
 };
